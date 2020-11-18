d7689df68191b89f334587ffbed5c2d785735bd6
Youshin Kim <kim1415@purdue.edu>
2020-11-19 01:59:46 +0900

    Updates

---
+++ precache-manifest.59f6982e2fcc6b0e22b2ded21cb4a5af.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8d6de90a60258c1c2ae01562bbbe19",
+    "url": "/weai-charles.github.io/index.html"
+  },
+  {
+    "revision": "7e6fc46cf64c05248ebf",
+    "url": "/weai-charles.github.io/static/css/main.ad848255.chunk.css"
+  },
+  {
+    "revision": "a1aff45fff8ecab2d69d",
+    "url": "/weai-charles.github.io/static/js/2.2e6c938b.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/weai-charles.github.io/static/js/2.2e6c938b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6fc46cf64c05248ebf",
+    "url": "/weai-charles.github.io/static/js/main.e569984e.chunk.js"
+  },
+  {
+    "revision": "800b77d78770aaa9d23b",
+    "url": "/weai-charles.github.io/static/js/runtime-main.6e1d682b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/weai-charles.github.io/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/weai-charles.github.io/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/weai-charles.github.io/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/weai-charles.github.io/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/weai-charles.github.io/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/weai-charles.github.io/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/weai-charles.github.io/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "b4fc21f280b17a8dec4b30c787827e55",
+    "url": "/weai-charles.github.io/static/media/predict_1.b4fc21f2.png"
+  },
+  {
+    "revision": "7d1f6d9c5e3b6df03091e24ed52b5927",
+    "url": "/weai-charles.github.io/static/media/predict_2.7d1f6d9c.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/weai-charles.github.io/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
