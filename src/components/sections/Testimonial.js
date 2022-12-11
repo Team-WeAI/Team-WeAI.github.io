820c02e1c54aaddb8888b680e63f91e57bbe299f
Youshin Kim <youshin1411@gmail.com>
2022-12-12 00:03:16 +0900

    update Eric

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -109,6 +109,23 @@ const Testimonial = ({
               </div>
             </div>
 
+          <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                  University of Maryland B.S in Computer Science
+                    </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">Eric Cho</span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">AI Engineer</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
